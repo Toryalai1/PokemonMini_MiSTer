100467841a1c3b4700f613ba6fb026a428ebb353
Toya <82502378+Toryalai1@users.noreply.github.com>
2024-04-08 08:27:27 +0200

    Try new builder

--- pokemon_mini.sv
+++ pokemon_mini.sv
@@ -211,7 +211,7 @@ assign VIDEO_ARY = (!ar) ? 12'd3 : 12'd0;
 
 `include "build_id.v"
 localparam CONF_STR = {
-    "pokemon_mini;;",
+    "PokemonMini;;",
     "-;",
     "FS1,min,Load ROM;",
     "-;",
